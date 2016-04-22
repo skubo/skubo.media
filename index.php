0941d74cadccaff6363fdf7b343badcd8e6e69cb
Christian Garbers <chriga@Christians-iMac.fritz.box>
2016-04-22 22:16:41 +0200

    some index

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8" />
+<title>skubo.media</title>
+
+<meta name="keywords"
+        content="music, video, sound, text, writing, podcasting, blog, blogging, media" />
+<meta name="description"
+        content="The creative hub of Chris Garbers - everthing skubo." />
+<meta name="author" content="Chris Garbers" />
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+<link rel="apple-touch-icon" href="/static/images/app-logo-64px.png" />
+<link rel="apple-touch-icon-precomposed" href="/static/images/app-logo-64px.png" />
+<link rel="stylesheet" href="index.css" type="text/css" />
+
+</head>
+<body>
+</body>
+</html>
