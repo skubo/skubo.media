a5c13306b24954048349968005aaeba6a252a726
Chris Garbers <skubo08@gmail.com>
2016-04-24 15:52:24 +0200

    another page image links and iframe support

--- element.php
+++ element.php
@@ -8,7 +8,14 @@ function layoutElement($pagetype, $element, $numcols, $col) {
 				echo 'left:'.(2+(100*($numcols-1)/$numcols)+($col-1)*4).'%;';
 			}
 			echo '">';
-			echo '<img src="'.$element->{'src'}.'" width="'.$element->{'width'}.'" height="'.$element->{'height'}.'" /></div>'."\n";
+			$link = $element->{'link'};
+			if ($link) {
+				echo '<a href="'.$link.'" target="_new">';
+			}
+			echo '<p class="vignette"><img src="'.$element->{'src'}.'" style="width:'.$element->{'width'}.';height:'.$element->{'height'}.';" /></p></div>'."\n";
+			if ($link) {
+				echo '</a>';
+			}
 			break;
 		case 'text':
 			echo '<div class="'.$pagetype.' text" style="font-size:'.$element->{'font-size'}.';';
@@ -27,6 +34,16 @@ function layoutElement($pagetype, $element, $numcols, $col) {
 			}
 			echo '">';
 			echo $element->{'channel'}."</div>"."\n";
+			break;
+		case 'iframe':
+			echo '<div class="'.$pagetype.' iframe" style="';
+			if ($numcols > 1) {
+				echo 'width:'.((100/$numcols)-4).'%;';
+				echo 'left:'.(2+(100*($numcols-1)/$numcols)+($col-1)*4).'%;';
+			}
+			echo '">';
+			echo '<iframe src="'.$element->{'src'}.'" style="width:'.$element->{'width'}.';height:'.$element->{'height'}.';" />';
+			echo "</div>"."\n";
 			break;
 		default:
 			# code...
