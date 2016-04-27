ab6d14efa3ca6b1e488ce513457a3a8d8639c571
Chris Garbers <skubo08@gmail.com>
2016-04-27 19:12:52 +0200

    I got my mojo, erm, REST interface working

--- pages/index.php
+++ pages/index.php
@@ -3,9 +3,10 @@ header('Content-type: application/json');
 // TODO authenticate
 
 // cumulate content
-$obj = file_get_contents('./content.json');
+$content = json_decode(file_get_contents('./content.json'));
+$pages = json_encode($content->{'pages'});
 
-$json = "{httpStatus:200,obj={".$obj."}}";
+$json = '{"httpStatus":200,"pages":'.$pages.'}';
 
 // zack return
 echo $json;
